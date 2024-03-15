b904bbd7f61a341c08e425975a9dcc57da36b07f
tiketdatasofianhadiant0 <sofian.hadianto@tiket.com>
2024-03-15 10:00:25 +0700

    sample tf

---
+++ main.tf
@@ -0,0 +1,40 @@
+provider "google" {
+    project = "learning-terraform-220783"
+    credentials = "${file("credentials.json")}"
+    region = "asia-southeast2"
+    zone = "asia-southeast2-c"
+}
+
+resource  "google_compute_instance" "my_instance" {
+    name = "agoda-instance-1"
+    machine_type = "e2-micro"
+    zone = "asia-southeast2-c"
+    allow_stopping_for_update = true
+    
+    boot_disk {
+      initialize_params {
+        image = "debian-cloud/debian-11"
+      }
+    }
+
+    network_interface {
+    #   network = "default"
+      network = google_compute_network.terraform_network.self_link
+      subnetwork = google_compute_subnetwork.terraform_subnet.self_link
+      access_config {
+        // neccessary even empty
+      }
+    }
+}
+
+resource "google_compute_network" "terraform_network" {
+  name = "terraform-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "terraform_subnet" {
+  name = "terraform-subnetwork"
+  ip_cidr_range = "10.185.0.0/16"
+  region = "asia-southeast2"
+  network = google_compute_network.terraform_network.id
+}
